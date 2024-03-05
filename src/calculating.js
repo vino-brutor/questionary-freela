be36aca352611aa239bf23e354621f3f21a04a9f
Vitor Bruno <viktorbruno2006@gmail.com>
2024-03-05 18:12:54 -0300

    novas atualizações

--- src/calculating.js
+++ src/calculating.js
@@ -1,27 +1,62 @@
 const form = document.getElementById('form')
 const reset = document.getElementById('reset-button')
-const person = document.getElementById('input-person').value
 const main = document.getElementsByTagName('main')
 let question
-let points = 0
+let sectionX = 0
+let sectionY = 0
+let resultX = 0
+let resultY = 0
 
 export const calculatingPoints = () => {
   
   const person = document.getElementById('input-person').value
-  for (let index = 1; index < 9; index++) {
-      
-    question = document.querySelector(`input[name="question${index}"]:checked`).value
-    if(question === 'optionA'){
-      points += 0
-    }else if(question === 'optionB'){
-      points += 4
-    }else if(question === 'optionC'){
-      points += 8 
+  for (let index = 1; index < 17; index++) {
+    
+    if(index % 2 === 0){
+      question = document.querySelector(`input[name="question${index}"]:checked`).value
+      if(question === 'optionA'){
+        sectionY += 0
+      }else if(question === 'optionB'){
+        sectionY += 1
+      }else if(question === 'optionC'){
+        sectionY += 2 
+      }
+    }else if(index % 2 !== 0){
+      question = document.querySelector(`input[name="question${index}"]:checked`).value
+      if(question === 'optionA'){
+        sectionX += 0
+      }else if(question === 'optionB'){
+        sectionX += 1
+      }else if(question === 'optionC'){
+        sectionX += 2 
     }
+    } 
+
   }
-  console.log(person, points, question)
-  const result = document.createElement('h1')
-  result.innerText = person + ' atingiu a pontuação de: '+ points
-  form.appendChild(result)  
+  resultCalculus(sectionX, sectionY)
+  reusltText()
 }
   
+
+
+const reusltText = () => {
+  const person = document.getElementById('input-person').value
+  const result = document.createElement('h1')
+  const ul = document.createElement('ul')
+  const liY = document.createElement('li')
+  const liX = document.createElement('li')
+
+  result.innerText = ('o entrevistado' ?? person) + ' atingiu a seguinte pontuação: '
+  liX.innerText = 'Seção X: ' + resultX + '%'
+  liY.innerText = 'Seção Y: ' + resultY + '%'
+
+  form.appendChild(result)
+  result.appendChild(ul)
+  ul.appendChild(liX)
+  ul.appendChild(liY) 
+}
+
+const resultCalculus = (X, Y) => {
+  resultX = (X/16) * 100
+  resultY = (Y/16) * 100
+}
